472228c4787e17b0785c0598683c4b13bb08f9e9
ROHIT NIGAM <shaw-in-030@ROHIT-MAC.local>
2016-01-14 11:41:54 +0530

    Receiving and sanitizing data

--- books-signup-webhook.php
+++ books-signup-webhook.php
@@ -11,7 +11,33 @@ $first_name = $data["first_name"];
 $last_name = $data["last_name"];
 $email = $data["email"];
 $country = $data["country"];
-$phone = $data
+$phone = $data["phone"];
+$age = $data["age"];
+$gender = $data["gender"];
+$industry = $data["industry"];
+$experience = $data["experience"];
+$lesson = $data["lesson"];
+
+$create_date = filter_var($create_date, FILTER_SANITIZE_NUMBER_INT);
+$first_name = filter_var($first_name, FILTER_SANITIZE_STRING);
+$last_name = filter_var($first_name, FILTER_SANITIZE_STRING);
+
+$country = filter_var($country, FILTER_SANITIZE_STRING);
+$phone = filter_var($phone, FILTER_SANITIZE_NUMBER_INT);
+$age = filter_var($age, FILTER_SANITIZE_NUMBER_INT);
+$gender = filter_var($gender, FILTER_SANITIZE_STRING);
+$industry = filter_var($industry, FILTER_SANITIZE_STRING);
+$experience = filter_var($experience, FILTER_SANITIZE_STRING);
+$lesson = filter_var($lesson, FILTER_SANITIZE_STRING);
+
+if (isset($email)) {
+	$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+		$email = "";
+}
+
+$phone = preg_replace("/[^0-9]/", '', $phone);
+if (!strlen($phone) == 10) $phone = '';
 
 
 
